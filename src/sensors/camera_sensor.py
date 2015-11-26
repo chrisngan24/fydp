5fe7e03505c00fbec3eeaa97db5a7b1213031181
Clement Fung <clement.y.fung@gmail.com>
2015-11-25 22:14:17 -0500

    Implement retinex and function calls into sensor code

--- src/sensors/camera_sensor.py
+++ src/sensors/camera_sensor.py
@@ -2,6 +2,7 @@ import cv2
 import logging
 import time
 import numpy as np
+import colorcorrect.algorithm as cca
 
 from sensor import BaseSensor 
 from sensor import SensorMaster
@@ -44,54 +45,78 @@ class CameraSensor(BaseSensor):
         self.nose_cascade = cv2.CascadeClassifier(self.nose_model_file)
         self.profile_cascade = cv2.CascadeClassifier(self.profile_model_file)
 
+    def get_features(self, gray):
+
+        faces = self.face_cascade.detectMultiScale(image = gray, 
+                scaleFactor = 1.3, 
+                minNeighbors = 3, 
+                flags = 0, 
+                minSize= (100,100))
+        eyes = []
+        noses = []
+
+        for (x,y,w,h) in faces:
+
+            roi_23down = gray[y+(h/3):y+h, x:x+w]
+            roi_23up = gray[y:y+(2*h/3), x:x+w]
+
+            eyes = self.eye_cascade.detectMultiScale(image = roi_23up, 
+                scaleFactor = 1.1, 
+                minNeighbors = 5, 
+                flags = 0)
+
+            noses = self.nose_cascade.detectMultiScale(image = roi_23down, 
+                scaleFactor = 1.1, 
+                minNeighbors = 3, 
+                flags = 0)
+
+        return (faces, eyes, noses)
+
     def find_new_KLT(self):
 
         face_found = False
+
         while(not face_found):
 
             ret,frame = self.camera.read()
             if ret == True:
 
+                frame = cca.stretch(frame)
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                # TODO: Move all these to consts
-                faces = self.face_cascade.detectMultiScale(image = gray, 
-                    scaleFactor = 1.3, 
-                    minNeighbors = 3, 
-                    flags = 0, 
-                    minSize= (100,100))
+                (faces, eyes, noses) = self.get_features(gray)
 
+                eye_mask = np.zeros(gray.shape)
                 nose_mask = np.zeros(gray.shape)
 
                 for (x,y,w,h) in faces:
 
-                    roi_gray = gray[y:y+h, x:x+w]
-                    eyes = self.eye_cascade.detectMultiScale(image = roi_gray, 
-                        scaleFactor = 1.1, 
-                        minNeighbors = 5, 
-                        flags = 0)
+                    print "Found face " + str(len(eyes)) + " " + str(len(noses))
 
-                    nose = self.nose_cascade.detectMultiScale(image = roi_gray, 
-                        scaleFactor = 1.15, 
-                        minNeighbors = 8, 
-                        flags = 0,
-                        minSize = (20,20))
-
-                    if (len(eyes) == 2 and len(nose) == 1):
+                    if (len(eyes) == 2 and len(noses) == 1):
 
                         face_found = True
                         self.old_frame = frame
 
-                        for (nx,ny,nw,nh) in nose:
+                        for (ex,ey,ew,eh) in eyes:
+                            
+                            # ex and ey are relative to the face frame, need to shift to entire frame
+                            tx = ex+x
+                            ty = ey+y
+                            eye_mask[ty:ty+eh, tx:tx+ew] = 1.
+
+                        for (nx,ny,nw,nh) in noses:
 
                             # ex and ey are relative to the face frame, need to shift to entire frame
                             tx = nx+x
-                            ty = ny+y
+                            ty = ny+y+h/3
                             nose_mask[ty:ty+nh, tx:tx+nw] = 1.
 
                         nose_mask = nose_mask.astype(np.uint8)
+                        eye_mask = eye_mask.astype(np.uint8)
                         break
 
+        # Take first frame and find corners in it
         old_gray = cv2.cvtColor(self.old_frame, cv2.COLOR_BGR2GRAY)
         self.p0_nose = cv2.goodFeaturesToTrack(old_gray, mask = nose_mask, **self.feature_params)
 
@@ -106,20 +131,23 @@ class CameraSensor(BaseSensor):
         # 1. We have KLT points which are stable and within the Viola-Area
         # 2. We have no KLT points, but we have a face. Initialize KLT.
 
+        frame = cca.stretch(frame)
         this_event = dict(
+            time=time.time(),
             isFrontFace=0,
-            faceX=-1,
-            faceY=-1,
+            faceLeft=-1,
+            faceRight=-1,
+            faceTop=-1,
+            faceBottom=-1,
             noseX=-1,
-            noseY=-1
+            noseY=-1,
             )
      
-        if (len(self.p0_nose) < 4):
+        if (len(self.p0_nose) < 3):
             print "Only " + str(len(self.p0_nose)) + " points left. Reinitializing.. " + str(time.time())
             self.find_new_KLT()
 
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
-
         old_gray = cv2.cvtColor(self.old_frame, cv2.COLOR_BGR2GRAY)        
         p1_nose, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, self.p0_nose, None, **self.lk_params)
 
@@ -137,7 +165,7 @@ class CameraSensor(BaseSensor):
         ################################################################################
 
         ### Viola-Jones : Regional Detection
-        faces = self.face_cascade.detectMultiScale(frame_gray, 1.2, 1, 3, (100,100))
+        (faces,eyes,noses) = self.get_features(frame_gray)
 
         if (len(faces) == 1):
 
@@ -145,8 +173,10 @@ class CameraSensor(BaseSensor):
 
             this_event.update(dict(
                 isFrontFace=1,
-                faceX=(x+w/2),
-                faceY=(y+h/2)
+                faceLeft=x,
+                faceRight=(x+w),
+                faceTop=(y),
+                faceBottom=(y+h)
                 ))
 
             # Define the ROI for eyes and noise
