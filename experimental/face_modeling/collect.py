28b60231f63678410c3d7db37eb18a3ade2cc227
Chris Ngan <chris.ngan24@gmail.com>
2016-01-20 16:56:58 -0500

    Added the option to pass a parameter when collecting face data

--- experimental/face_modeling/collect.py
+++ experimental/face_modeling/collect.py
@@ -8,6 +8,10 @@ import klt
 
 if __name__ == '__main__':
     sub_dir = sys.argv[1]
+    if len(sys.argv) == 3:
+        file_name = sys.argv[2]
+    else:
+        file_name = str(int(time.time()))
     base_dir = 'data'
     output_dir = '%s/%s' % (base_dir, sub_dir)
     if not os.path.exists(output_dir) or not os.path.isdir(output_dir):
@@ -16,7 +20,7 @@ if __name__ == '__main__':
 
     output_file = '%s/%s.csv' % (
             output_dir, 
-            str(int(time.time())),
+            file_name,
             )
     df = None
     events = []
@@ -24,13 +28,9 @@ if __name__ == '__main__':
         df = klt.run(events)
     finally:
         print events
-        if df == None:
-            df = pd.DataFrame(events)
         if len(df) > 0:
             print 'end data collection'
             print df.head()
+            print 'copying data to: ' + file_name
             df.to_csv(output_file, index=False)
-
-
-        print 'copying data to: ', 
         
